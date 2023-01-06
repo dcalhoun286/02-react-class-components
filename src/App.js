c27b5f94a6abda8f2fe12f20daf847cb3075acce
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2023-01-06 09:51:00 -0800

    demo for class components intro complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Photo } from './Photo';
 
 const IMAGE_URL = "https://picsum.photos/350";
 const API_URL = "https://catfact.ninja/fact";
-function App () {
 
-    
-    return (
-         <h1>Hello World!</h1>
-    )
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            catFact: 'test fact'
+        };
+    }
+
+    componentDidMount() {
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => this.setState({catFact: data.fact}))
+    }
+
+    render(){
+        return (
+            <>
+                <h1>{this.state.catFact}</h1>
+                <Photo src={IMAGE_URL} />
+            </>
+        )
+    }
 
 }
 
--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function Photo () {
-  
-}
+class Photo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-export { Photo };
+        };
+    }
+
+    render() {
+        return (
+            <img width="350" height="350" src={this.props.src} />
+        );
+    };
+        
+}
+export { Photo }; 
