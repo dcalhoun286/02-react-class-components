8027d3ab268d9ca396a54f65113f7cc6979ad2b2
Kat Law <kathleen.law@multiverse.io>
2023-01-05 20:50:17 -0500

    more cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const IMAGE_URL = "https://picsum.photos/350";
+const API_URL = "https://catfact.ninja/fact";
 function App () {
 
-
+    
     return (
-         <h1>'Hello World!'</h1>
+         <h1>Hello World!</h1>
     )
 
 }
--- src/Photo.js
+++ src/Photo.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-class Photo extends React.Component {
-  
-
+function Photo () {
   
 }
 
